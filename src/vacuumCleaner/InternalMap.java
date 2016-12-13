7deba5cacceddfa1912979bafa4f36d936d3cdcd
teguayco <alu0100825503@ull.edu.es>
2016-12-13 15:44:48 +0200

    añadiendo clase InternalMap

---
+++ src/vacuumCleaner/InternalMap.java
@@ -0,0 +1,15 @@
+package vacuumCleaner;
+
+/**
+ * Clase que representa el mapa interno del robot 
+ * (representado en la interfaz en la esquina inferior
+ * izquierda)
+ * @author Teguayco
+ */
+public class InternalMap {
+	
+	
+	public InternalMap() {
+		
+	}
+}
