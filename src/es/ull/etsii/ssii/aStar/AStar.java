35fd63e7b82af5439f3dabe274615bf9ca4725d9
Rubén <alu0100309553@ull.edu.es>
2017-01-20 13:02:14 +0000

    Arreglado fallo en A*

--- src/es/ull/etsii/ssii/aStar/AStar.java
+++ src/es/ull/etsii/ssii/aStar/AStar.java
@@ -82,16 +82,16 @@ public class AStar {
 	
 	private ArrayList <Node> adjacent(Node o){
     ArrayList<Node> aux = new ArrayList <Node>();
-    if (!up(o).isOpaque && !closeList.contains(up(o))){
+    if (up(o) != null && !up(o).isOpaque && !closeList.contains(up(o))){
       aux.add(up(o));
     }
-    if (!down(o).isOpaque  && !closeList.contains(down(o))){
+    if (down(o) != null && !down(o).isOpaque  && !closeList.contains(down(o))){
       aux.add(down(o));
     }
-    if (!left(o).isOpaque  && !closeList.contains(left(o))){
+    if (left(o) != null && !left(o).isOpaque  && !closeList.contains(left(o))){
       aux.add(left(o));
     }
-    if (!right(o).isOpaque && !closeList.contains(right(o))){
+    if (right(o) != null && !right(o).isOpaque && !closeList.contains(right(o))){
       aux.add(right(o));
     }
     
@@ -108,7 +108,7 @@ public class AStar {
 	}
 	
 	private Node down (Node o){
-    if (o.yCoord < map [0].length-1){
+    if (o.yCoord < map.length-1){
       return map [o.xCoord][o.yCoord+1];
     } else {
       return null;
@@ -124,7 +124,7 @@ public class AStar {
   }
 	
 	private Node right (Node o){
-	  if (o.yCoord < map.length-1){
+	  if (o.yCoord < map[0].length-1){
       return map [o.xCoord+1][o.yCoord];
     } else {
       return null;
@@ -135,7 +135,4 @@ public class AStar {
 		return Math.abs(o.xCoord-objetive.xCoord)+Math.abs(o.yCoord-objetive.yCoord);
 	}
 	
-	private int f(){
-		return 1;
-	}
 }
